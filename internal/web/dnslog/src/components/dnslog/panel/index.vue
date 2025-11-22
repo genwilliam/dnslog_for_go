bf947f9d3a1927604cb142a8354e099c9d9e5068
william <genwillen@gmail.com>
2025-11-22 19:46:55 +0800

    encapsulate button

--- internal/web/dnslog/src/components/dnslog/panel/index.vue
+++ internal/web/dnslog/src/components/dnslog/panel/index.vue
@@ -1,30 +1,33 @@
 <template>
-  <div class="panel">
-    <inp class="inp" />
-    <btn class="btn" />
-    <result />
-  </div>
+  <n-config-provider>
+    <n-loading-bar-provider>
+      <n-message-provider>
+        <n-notification-provider>
+          <n-modal-provider>
+            <n-dialog-provider>
+              <div class="btn-group">
+                <generateBtn />
+                <stateBtn />
+                <dataPersis />
+              </div>
+            </n-dialog-provider>
+          </n-modal-provider>
+        </n-notification-provider>
+      </n-message-provider>
+    </n-loading-bar-provider>
+  </n-config-provider>
 </template>
 
-<script lang="ts" setup>
-import Inp from '@/components/dnslog/input/index.vue';
-import Btn from '@/components/dnslog/button/index.vue';
-import result from '@/views/result/index.vue';
+<script setup lang="ts">
+import generateBtn from '@/components/dnslog/button/generate.vue';
+import queryBtn from '@/components/dnslog/button/query.vue';
+import stateBtn from '@/components/dnslog/button/state.vue';
+import dataPersis from '@/components/dnslog/button/persis.vue';
 </script>
 
 <style scoped>
-.panel {
+.btn-group {
   display: flex;
-  flex-direction: column;
-  gap: 16px; /* 控制上下间距 */
-
-  width: 668px;
-  min-height: 325px;
-  border: 1px solid #ccc;
-  padding: 16px;
-  border-radius: 8px;
-  box-sizing: border-box;
-  resize: vertical;
-  overflow: auto;
+  gap: 12px;
 }
 </style>
